94af9bb6dd156ce98b548398624f8ffd633a3a20
alstlr0312 <90487082+alstlr0312@users.noreply.github.com>
2022-07-07 17:34:18 +0900

    login_logout_createAccont_noAccountLogin

--- app/src/main/java/com/example/dangdangee/MainActivity.kt
+++ app/src/main/java/com/example/dangdangee/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.dangdangee
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import androidx.databinding.DataBindingUtil
-import com.example.dangdangee.databinding.ActivityMainBinding
+import com.example.dangdangee.auth.IntroActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -15,43 +13,17 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        // Initialize Firebase Auth
         auth = Firebase.auth
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        findViewById<Button>(R.id.logoutBtn).setOnClickListener {
+            auth.signOut()
 
-        val joinBtnCliced = findViewById<Button>(R.id.joinBtn)
-        joinBtnCliced.setOnClickListener {
-
-
-            //첫번째 방법
-            //val email = findViewById<EditText>(R.id.emailArea)
-            //val pwd = findViewById<EditText>(R.id.pwdArea)
-
-            //두번째 방법
-            val email=binding.emailArea
-            val pwd = binding.pwdArea
-
-            auth.createUserWithEmailAndPassword(
-                email.text.toString(),
-                pwd.text.toString()
-
-            )
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        Toast.makeText(this,"ok",Toast.LENGTH_SHORT).show()
-                    } else {
-                        Toast.makeText(this,"no",Toast.LENGTH_SHORT).show()
-                    }
-                }
-
+            val intent = Intent(this, IntroActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or  Intent.FLAG_ACTIVITY_CLEAR_TOP //회원가입하면 뒤에있는 엑티비티 없애기
+            startActivity(intent)
         }
     }
 }
