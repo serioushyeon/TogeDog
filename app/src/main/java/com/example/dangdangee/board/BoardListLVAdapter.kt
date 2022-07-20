d3dcc3c3c905e2d66476795bca08e6fe207b02c0
alstlr0312 <alstlr0312@naver.com>
2022-07-20 16:30:52 +0900

    board+++

--- app/src/main/java/com/example/dangdangee/board/BoardListLVAdapter.kt
+++ app/src/main/java/com/example/dangdangee/board/BoardListLVAdapter.kt
@@ -29,6 +29,12 @@ class BoardListLVAdapter(val boardList : MutableList<BoardModel>) : BaseAdapter(
         val title = view?.findViewById<TextView>(R.id.titleArea)
         title!!.text = boardList[position].title
 
+        val content = view?.findViewById<TextView>(R.id.contentArea)
+        content!!.text = boardList[position].content
+
+        val time = view?.findViewById<TextView>(R.id.timeArea)
+        time!!.text = boardList[position].time
+
         return view!!
     }
 }
