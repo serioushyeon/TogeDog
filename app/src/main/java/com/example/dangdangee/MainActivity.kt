c6cddd8873ce3d061b299a2e8e01febfd11fa5b1
alstlr0312 <alstlr0312@naver.com>
2022-07-26 18:13:45 +0900

    umm,,,

--- app/src/main/java/com/example/dangdangee/MainActivity.kt
+++ app/src/main/java/com/example/dangdangee/MainActivity.kt
@@ -1,41 +1,45 @@
 package com.example.dangdangee
 
-import android.content.Intent
+
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
+import android.view.WindowManager
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import com.example.dangdangee.auth.IntroActivity
-import com.example.dangdangee.board.Board
-import com.example.dangdangee.board.BoardWriteActivity
-import com.example.dangdangee.map.MapViewModel
-import com.example.dangdangee.map.MarkerRegisterActivity
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+
+import com.example.dangdangee.databinding.ActivityMainBinding
+import com.example.dangdangee.databinding.ActivityMainMapBinding
+
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.naver.maps.geometry.LatLng
-import com.naver.maps.geometry.LatLngBounds
-import com.naver.maps.map.LocationTrackingMode
-import com.naver.maps.map.MapView
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
-import com.naver.maps.map.overlay.Marker
-import com.naver.maps.map.overlay.Overlay
-import com.naver.maps.map.overlay.OverlayImage
-import com.naver.maps.map.util.FusedLocationSource
 
 
 class MainActivity : AppCompatActivity(){
-
-
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         auth = Firebase.auth
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        setStatusBarColor(R.color.black)
+    }
 
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment_view)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun setStatusBarColor(color: Int) { // 상단 메뉴바 색 바꾸는 함수
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+
+        // finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this, color))
+    }
 
 }
