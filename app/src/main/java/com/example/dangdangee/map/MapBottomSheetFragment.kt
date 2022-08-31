67dc57b5bddf17eb862a423aef0944bc86afb4dd
alstlr0312 <alstlr0312@naver.com>
2022-08-31 21:17:09 +0900

    지도와 게시글 연결

--- app/src/main/java/com/example/dangdangee/map/MapBottomSheetFragment.kt
+++ app/src/main/java/com/example/dangdangee/map/MapBottomSheetFragment.kt
@@ -1,10 +1,12 @@
 package com.example.dangdangee.map
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.OvalShape
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,10 +15,21 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.view.isVisible
 import com.example.dangdangee.R
+import com.example.dangdangee.Utils.FBRef
+import com.example.dangdangee.board.BoardModel
+import com.example.dangdangee.board.PostActivity
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MapBottomSheetFragment : BottomSheetDialogFragment() {
-
+    private val boardKeyList = arrayListOf<String>()
+    private val boardDataList = arrayListOf<BoardModel>()
+    private lateinit var mapref: DatabaseReference
     lateinit var name : String
     lateinit var address : String
     lateinit var breed : String
@@ -35,9 +48,13 @@ class MapBottomSheetFragment : BottomSheetDialogFragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        mapref = Firebase.database.getReference("Marker")
         val imgview = view.findViewById<ImageView>(R.id.info_image_pet)
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<Button>(R.id.info_to_post).setOnClickListener {
+            val intent = Intent(context, PostActivity::class.java)
+            intent.putExtra("key",key)
+            startActivity(intent)
             //getSupportFragmentManager().beginTransaction().replace(컨테이너, 게시글프래그먼트).commit();
         }
         if(flag){
@@ -57,5 +74,22 @@ class MapBottomSheetFragment : BottomSheetDialogFragment() {
         imgview.setImageDrawable(img)
         imgview.background = ShapeDrawable(OvalShape())
         imgview.clipToOutline = true
+    }
+    fun getData(){
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for(dataModel in dataSnapshot.children){
+                    val item = dataModel.getValue(BoardModel::class.java)
+                    boardDataList.add(item!!)
+                    boardKeyList.add(dataModel.key.toString())
+                }
+                boardKeyList.reverse()
+                boardDataList.reverse()
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FBRef.boardRef.addValueEventListener(postListener)
     }
 }
