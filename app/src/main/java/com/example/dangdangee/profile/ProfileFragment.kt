1366afc53eebd3c88fa4f4d3d4ae9e3332a2b943
Yunjeong Cho <dbswjd1413@naver.com>
2022-09-10 01:52:26 +0900

    updating design6

--- app/src/main/java/com/example/dangdangee/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/dangdangee/profile/ProfileFragment.kt
@@ -6,20 +6,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import com.example.dangdangee.auth.LoginActivity
 import com.example.dangdangee.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class ProfileFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): FrameLayout{
         val binding = FragmentProfileBinding.inflate(inflater,container,false)
-        binding.profileEdit.setOnClickListener {
-            startActivity(Intent(activity,ProfileEditActivity::class.java))
+        auth = Firebase.auth
+        binding.textView9.setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(activity,LoginActivity::class.java))
+
         }
         return binding.root
     }
-
 }
